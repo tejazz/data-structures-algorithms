533058cfcf749d44c6a2b8687edfbfa2e4a3fc8c
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-09-22 01:10:03 +0700

    array functions

--- array-interview.js
+++ array-interview.js
@@ -72,8 +72,121 @@
 
 // MAXIMUM SUB ARRAY
 
-var maxSubArray = function(nums) {
-    
-};
+// var maxSubArray = function(nums) {
+//     // check for input
+//     if (nums.length === 0 || nums.length === 1) {
+//         return nums[0] || 0;
+//     }
 
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+//     let i = 0;
+//     let sumMax = nums[i];
+//     let arrMax = [];
+
+//     while(i < nums.length) {
+//         let subSum = nums[i];
+//         let subMax = nums[i];
+//         let runArr = [nums[i]];
+//         let maxArr = [];
+
+//         for (let j = i + 1; j < nums.length; j++) {
+//             subSum += nums[j];
+//             runArr.push(nums[j]);
+
+//             if (subMax < subSum) {
+//                 subMax = subSum;
+//                 maxArr = runArr;
+//             }
+//         }
+
+//         // check with main branch
+//         if (subMax > sumMax) {
+//             sumMax = subMax;
+//             arrMax = maxArr;
+//         }   
+//         i++;
+//     }
+
+//     return sumMax;
+// };
+
+// O(n*2) => Slow
+
+// Faster alternative
+// var maxSubArray = function (nums) {
+//     let max1 = nums[0];
+//     let max2 = nums[0];
+//     for (let i = 1; i < nums.length; i++) {
+//         max1 = Math.max(nums[i], max1 + nums[i])
+
+//         if (max1 > max2) {
+//             max2 = max1;
+//         }
+//     }
+//     return max2;
+// }
+
+// console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+
+// MOVE ZEROES
+
+// var moveZeroes = function(nums) {
+//     for (let i =nums.length - 1; i >= 0; i--) {
+//         if (nums[i] === 0) {
+//             nums.splice(i, 1);
+//             nums.push(0);
+//         }
+//     }
+
+//     return nums;
+// };
+
+// console.log(moveZeroes([0, 0, 1]));
+
+// CONTAINS DUPLICATES
+
+// var containsDuplicate = function(nums) {
+//     let mapSet = {};
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (mapSet.hasOwnProperty(nums[i])) {
+//             return true;
+//         }
+
+//         mapSet[nums[i]] = true;
+//     }
+
+//     return false;
+// };
+
+// Single Line Faster Alternative
+
+// var containsDuplicate = function(nums) {
+//     return !(nums.length == new Set(nums).size);
+// }
+
+// console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
+
+// ROTATE ARRAY
+
+// var rotate = function (nums, k) {
+//     let i = 0;
+
+//     while (i < 3) {
+//         nums.unshift(nums[nums.length - 1]);
+//         nums.splice(nums.length - 1, 1);
+//         i++;
+//     }
+
+//     return nums;
+// };
+
+// alternative
+
+// var rotate = function (nums, k) {
+//     let section = nums.slice(nums.length - k, nums.length);
+//     let main = nums.slice(0, nums.length - k);
+
+//     return [...section, ...main];
+// }
+
+// console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3))
