d55cba85a0fe86278cc6dadae4839fc04b2c5b3a
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-09-23 12:16:45 +0700

    hash collisions prevention

--- hash-table-proto.js
+++ hash-table-proto.js
@@ -33,4 +33,29 @@ class HashTable {
 
         return undefined;
     }
+
+    keys() {
+        const keysArray = [];
+        for (let i = 0; i < this.data.length; i++) {
+            // preventing hash collision
+            if (this.data[i] && this.data[i].length) {
+                // but also loop through all the potential collisions
+                if (this.data.length > 1) {
+                    for (let j = 0; j < this.data[i].length; j++) {
+                        result.push(this.data[i][j][0])
+                    }
+                } else {
+                    result.push(this.data[i][0])
+                }
+            }
+        }
+        return keysArray;
+    }
 }
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+myHashTable.set('apples', 54);
+myHashTable.set('oranges', 2);
+myHashTable.keys();
+console.log(JSON.stringify(myHashTable));
