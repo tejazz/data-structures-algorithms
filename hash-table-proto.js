5e05bd2a2a0172484a20e921fe0bd24b15f0b838
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-09-23 12:05:49 +0700

    hashtable proto

---
+++ hash-table-proto.js
@@ -0,0 +1,36 @@
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size);
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+        }
+
+        return hash;
+    }
+
+    set(key, value) {
+        let address = this._hash(key);
+        if (!this.data[address]) {
+            this.data[address] = [];
+        }
+
+        this.data[address].push([key, value]);
+        return this.data;
+    }
+
+    get(key) {
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        for (let i = 0; i < currentBucket; i++) {
+            if (currentBucket[0] === key) {
+                return currentBucket[1];
+            }
+        }
+
+        return undefined;
+    }
+}
