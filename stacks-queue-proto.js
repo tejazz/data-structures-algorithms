09aa54e1e35aad3a69f864d5b86a18aea40a9f1a
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-09-27 00:37:34 +0700

    queues using linked lists

--- stacks-queue-proto.js
+++ stacks-queue-proto.js
@@ -32,14 +32,16 @@
 // CALL STACK => CALLBACK 
 
 // STACK - Linked List Implementation
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
-}
+
+// class Node {
+//     constructor(value) {
+//         this.value = value;
+//         this.next = null;
+//     }
+// }
 
 // Linked list Implementation
+
 // class Stack {
 //     constructor() {
 //         this.top = null;
@@ -105,54 +107,138 @@ class Node {
 // } 
 
 // Array Implementation
-class Stack {
-    constructor() {
-        this.stack = new Array();
-        this.length = 0;
-    }
 
-    printStack() {
-        console.log(this.stack);
-    }
+// class Stack {
+//     constructor() {
+//         this.stack = new Array();
+//         this.length = 0;
+//     }
 
-    push(value) {
-        this.stack.unshift(value);
-        this.length++;
-        return this.printStack();
-    }
+//     printStack() {
+//         console.log(this.stack);
+//     }
 
-    pop() {
-        if (this.length === 0) {
-            return this.printStack();
-        }
+//     push(value) {
+//         this.stack.push(value);
+//         this.length++;
+//         return this.printStack();
+//     }
 
-        this.stack.splice(0, 1);
-        this.length--;
-        return this.printStack();
-    }
+//     pop() {
+//         this.stack.pop();
+//         this.length--;
+//         return this.printStack();
+//     }
 
-    peek() {
-        return this.stack[0];
-    }
+//     peek() {
+//         return this.stack[this.length - 1];
+//     }
 
-    isEmpty() {
-        if (this.length === 0) {
-            return console.log(true);
-        }
+//     isEmpty() {
+//         if (this.length === 0) {
+//             return console.log(true);
+//         }
 
-        return console.log(false);
-    }
-}
+//         return console.log(false);
+//     }
+// }
 
-let myStack = new Stack();
+// let myStack = new Stack();
 
 // Example Input:
 // Discord, Google, Udemy
-myStack.push('Discord');
-myStack.push('Google');
-myStack.push('Udemy');
-myStack.pop();
-myStack.peek();
-myStack.isEmpty();
+// myStack.push('Discord');
+// myStack.push('Google');
+// myStack.push('Udemy');
+// myStack.pop();
+// myStack.peek();
+// myStack.isEmpty();
 
-console.log(myStack);
+// console.log(myStack);
+
+// QUEUE
+
+// class Node {
+//     constructor(value) {
+//         this.value = value;
+//         this.next = null;
+//     }
+// }
+
+// class Queue {
+//     constructor() {
+//         this.first = null;
+//         this.last = null;
+//         this.length = 0;
+//     }
+
+//     peek() {
+//         console.log(this.first);
+//     }
+
+//     enqueue(value) {
+//         let newNode = new Node(value);
+
+//         if (!this.first) {
+//             this.first = newNode;
+//             this.last = newNode;
+//             this.length++;
+//             return this.printQueue();
+//         }
+
+//         this.last.next = newNode;
+//         this.last = newNode;
+//         this.length++;
+//         return this.printQueue();
+//     }
+
+//     dequeue() {
+//         if (this.length === 0) {
+//             return this.printQueue();
+//         }
+
+//         if (this.first === this.last) {
+//             this.last = null;
+//         }
+
+//         this.first = this.first.next;
+//         this.length--;
+//         return this.printQueue();
+//     }
+
+//     isEmpty() {
+//         if (this.length === 0) {
+//             return console.log(true);
+//         } 
+
+//         return console.log(false);
+//     }
+
+//     printQueue() {
+//         let currentNode = this.first;
+//         let flatArray = [];
+
+//         while (currentNode !== null) {
+//             flatArray.push(currentNode.value);
+//             currentNode = currentNode.next;
+//         }
+
+//         console.log(flatArray);
+//     }
+// }
+
+// const myQueue = new Queue();
+
+// Joy - Matt - Pavel - Samir
+// myQueue.enqueue('Jay');
+// myQueue.enqueue('Matt');
+// myQueue.enqueue('Pavel');
+// myQueue.enqueue('Samir');
+// myQueue.peek();
+// myQueue.dequeue();
+// myQueue.dequeue();
+// myQueue.dequeue();
+// myQueue.dequeue();
+// myQueue.isEmpty();
+
+// console.log(myQueue);
