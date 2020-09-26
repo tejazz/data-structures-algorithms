bcd88fc12aea9b8b52287a72ce74f1d788aef4dc
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-09-26 23:32:31 +0700

    array implementation of stack

--- stacks-queue-proto.js
+++ stacks-queue-proto.js
@@ -39,69 +39,110 @@ class Node {
     }
 }
 
+// Linked list Implementation
+// class Stack {
+//     constructor() {
+//         this.top = null;
+//         this.bottom = null;
+//         this.length = 0;
+//     }
+
+//     peek() {
+//         console.log(this.top);
+//     }
+
+//     pop() {
+//         if (!this.top) {
+//             return this.printStack();
+//         }
+
+//         let deleteNode = this.top;
+//         this.top = this.top.next;
+//         this.length--;
+
+//         if (this.length === 0) {
+//             this.bottom = null;
+//         }
+
+//         return this.printStack();
+//     }
+
+//     push(value) {
+//         let newNode = new Node(value);
+
+//         if (this.length === 0) {
+//             this.top = newNode;
+//             this.bottom = newNode;
+//             this.length++;
+//             return this.printStack();
+//         }
+
+//         newNode.next = this.top;
+//         this.top = newNode;
+//         this.length++;
+//         return this.printStack();
+//     }
+
+//     isEmpty() {
+//         if (!this.top) {
+//             return console.log(true);
+//         }
+
+//         return console.log(false);
+//     }
+
+//     printStack() {
+//         let currentNode = this.top;
+//         let flatArray = [];
+
+//         while(currentNode !== null) {
+//             flatArray.push(currentNode.value);
+//             currentNode = currentNode.next;
+//         }
+
+//         console.log(flatArray);
+//     }
+// } 
+
+// Array Implementation
 class Stack {
     constructor() {
-        this.top = null;
-        this.bottom = null;
+        this.stack = new Array();
         this.length = 0;
     }
 
-    peek() {
-        console.log(this.top);
-    }
-
-    pop() {
-        if (!this.top) {
-            return this.printStack();
-        }
-
-        let deleteNode = this.top;
-        this.top = this.top.next;
-        this.length--;
-
-        if (this.length === 0) {
-            this.bottom = null;
-        }
-
-        return this.printStack();
+    printStack() {
+        console.log(this.stack);
     }
 
     push(value) {
-        let newNode = new Node(value);
-
-        if (this.length === 0) {
-            this.top = newNode;
-            this.bottom = newNode;
-            this.length++;
-            return this.printStack();
-        }
-
-        newNode.next = this.top;
-        this.top = newNode;
+        this.stack.unshift(value);
         this.length++;
         return this.printStack();
     }
 
+    pop() {
+        if (this.length === 0) {
+            return this.printStack();
+        }
+
+        this.stack.splice(0, 1);
+        this.length--;
+        return this.printStack();
+    }
+
+    peek() {
+        return this.stack[0];
+    }
+
     isEmpty() {
-        if (!this.top) {
+        if (this.length === 0) {
             return console.log(true);
         }
 
         return console.log(false);
     }
-
-    printStack() {
-        let currentNode = this.top;
-        let flatArray = [];
-
-        while(currentNode !== null) {
-            flatArray.push(currentNode.value);
-            currentNode = currentNode.next;
-        }
-
-        console.log(flatArray);
-    }
-} 
+}
 
 let myStack = new Stack();
 
