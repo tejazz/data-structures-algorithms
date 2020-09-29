6a08c0cb8211dc70e365143aeab9e7097239b507
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-09-30 00:48:45 +0700

    bubble and selection sort

--- sorting-algorithms.js
+++ sorting-algorithms.js
@@ -29,7 +29,18 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function selectionSort(array) {
-  
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+
+    return numbers;
 }
 
 selectionSort(numbers);
+console.log(numbers);
