da73efd8d011d4bedac66adc779adf2b0f5d4d2c
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-09-30 23:02:23 +0700

    insertion sort added

--- sorting-algorithms.js
+++ sorting-algorithms.js
@@ -4,7 +4,7 @@
 // TC: O(n^2)
 // SC: O(1)
 
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+// const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 // function bubbleSort(array) {
 //     for (let i = 0; i < array.length; i++) {
@@ -26,21 +26,47 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 // Selection Sort
 // TC: O(n^2)
 
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+// const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function selectionSort(array) {
-    for (let i = 0; i < array.length; i++) {
-        for (let j = i + 1; j < array.length; j++) {
-            if (array[i] > array[j]) {
-                let temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
-        }
-    }
+// function selectionSort(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = i + 1; j < array.length; j++) {
+//             if (array[i] > array[j]) {
+//                 let temp = array[i];
+//                 array[i] = array[j];
+//                 array[j] = temp;
+//             }
+//         }
+//     }
 
-    return numbers;
-}
+//     return numbers;
+// }
 
-selectionSort(numbers);
-console.log(numbers);
+// selectionSort(numbers);
+// console.log(numbers);
+
+// Insertion Sort
+// TC: Best O(n), Worst O(n^2)
+
+// const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+// function insertionSort(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] < array[0]) {
+//             array.unshift(array.splice(i, 1)[0]);
+//         } else {
+//             // find where number should be placed 
+//             // in before section
+//             for (j = 1; j < i; j++) {
+//                 if (array[i] > array[j - 1] && array[i] < array[j]) {
+//                     array.splice(j, 0, array.splice(i, 1)[0]);
+//                 }
+//             }
+//         }
+//     }
+
+//     return numbers;
+// }
+
+// insertionSort(numbers);
+// console.log(numbers);
