1825568add754c2e8546cef4447c7c8bf54a55a5
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-09-30 23:22:54 +0700

    merge sort implementation

--- sorting-algorithms.js
+++ sorting-algorithms.js
@@ -47,6 +47,7 @@
 
 // Insertion Sort
 // TC: Best O(n), Worst O(n^2)
+// SC: O(1)
 
 // const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
@@ -70,3 +71,50 @@
 
 // insertionSort(numbers);
 // console.log(numbers);
+
+// Merge Sort and Quick Sort => Divide and Conquer
+// Divide and Conquer is normally O(n log n)
+
+// Merge Sort
+// TC: O(n log n)
+// SC: O(n)
+
+// const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+// function mergeSort (array) {
+//   if (array.length === 1) {
+//     return array
+//   }
+//   // Split Array in into right and left
+//   const length = array.length;
+//   const middle = Math.floor(length/2);
+//   const left = array.slice(0, middle);
+//   const right = array.slice(middle);
+
+//   return merge(
+//     mergeSort(left),
+//     mergeSort(right)
+//   )
+// }
+
+// function merge(left, right){
+//     let result = [];
+//     let leftIndex = 0;
+//     let rightIndex = 0;
+
+//     while(leftIndex < left.length  && rightIndex < right.length) {
+//         if (left[leftIndex] < right[rightIndex]) {
+//             result.push(left[leftIndex]);
+//             leftIndex++;
+//         } else {
+//             result.push(right[rightIndex]);
+//             rightIndex++;
+//         }
+//     }
+
+//     return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+// }
+
+
+// const answer = mergeSort(numbers);
+// console.log(answer);
