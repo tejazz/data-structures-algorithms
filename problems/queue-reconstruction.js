cfac5c2838fb4b04bea51c6c292615f1e9a36d4a
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-10-08 22:05:01 +0700

    queue reconstruction

--- problems/queue-reconstruction.js
+++ problems/queue-reconstruction.js
@@ -1,7 +1,14 @@
 // https://leetcode.com/problems/queue-reconstruction-by-height/
 
 var reconstructQueue = function(people) {
+    people.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]);
+    let final = [];
 
+    for (let i = 0; i < people.length; i++) {
+        final.splice(people[i][1], 0, people[i]);
+    }
+
+    return final;
 };
 
-console.log(reconstructQueue([[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]])); // [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+console.log(reconstructQueue([[9,0],[7,0],[1,9],[3,0],[2,7],[5,3],[6,0],[3,4],[6,2],[5,2]])); // [[3,0],[6,0],[7,0],[5,2],[3,4],[5,3],[6,2],[2,7],[9,0],[1,9]]
