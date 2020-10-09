37f9d8a43bdcb86eb6acee04a0bb09d74776562f
tejaz <Tarique.Ejaz@agoda.com>
2020-10-09 14:34:11 +0700

    added rob question

---
+++ problems/house-robber.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/house-robber/submissions/
+
+var rob = function (nums) {
+
+};
+
+console.log(rob([1])); // 1
+console.log(rob([1, 2, 3, 1])); // 4
+console.log(rob([2, 1, 1, 2])); // 4
+console.log(rob([1, 3, 1])); // 3
+console.log(rob([])); // 0
+console.log(rob([1, 2])); // 2
+console.log(rob([6,3,10,8,2,10,3,5,10,5,3])); // 29
