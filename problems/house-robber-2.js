c0947628dab9ef67b4ca5d473ac18596777377a8
tejaz <Tarique.Ejaz@agoda.com>
2020-10-14 15:38:55 +0700

    added question

---
+++ problems/house-robber-2.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/560/week-2-october-8th-october-14th/3494/
+
+var rob = function (nums) {
+
+};
+
+console.log(rob([2, 3, 2]));    // 3 
+console.log(rob([1, 2, 3, 1]));    // 4 
+console.log(rob([0]));    // 0
