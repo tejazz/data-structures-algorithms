e22ea1bc074ffac5d73ec41e1b83c85c49f23c31
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-10-16 13:39:41 +0700

    group the people

---
+++ problems/group-the-people.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/
+
+var groupThePeople = function(groupSizes) {
+    
+};
+
+console.log(groupThePeople([3,3,3,3,3,1,3]));   // [[5],[0,1,2],[3,4,6]]
+console.log(groupThePeople([2,1,3,3,3,2])); // [[1],[0,5],[2,3,4]]
