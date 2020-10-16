bc0603ec7aba1ec3bc7ddc8554dda6278e3ee898
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-10-16 18:54:55 +0700

    group people

--- problems/group-the-people.js
+++ problems/group-the-people.js
@@ -1,7 +1,27 @@
 // https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/
 
 var groupThePeople = function(groupSizes) {
-    
+    let subGroups = {};
+
+    for (let i = 0; i < groupSizes.length; i++) {
+        if (subGroups[groupSizes[i]]) {
+            let size = groupSizes[i];
+            let subGroupLength = subGroups[groupSizes[i]].length;
+            if (subGroups[groupSizes[i]][subGroupLength - 1].length < groupSizes[i]) {
+                subGroups[groupSizes[i]][subGroupLength - 1].push(i);
+            } else {
+                subGroups[groupSizes[i]].push([i]);
+            }
+            continue;
+        }
+
+        subGroups[groupSizes[i]] = [[i]];
+    }
+
+    let result = [];
+    Object.keys(subGroups).map(el => result = [...result, ...subGroups[el]]);
+
+    return result;
 };
 
 console.log(groupThePeople([3,3,3,3,3,1,3]));   // [[5],[0,1,2],[3,4,6]]
