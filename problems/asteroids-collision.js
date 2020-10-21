81c94cd921c5d7d2f1d6b3a26a170f31a225fd32
tejaz <Tarique.Ejaz@agoda.com>
2020-10-21 17:38:45 +0700

    asteroids collision

---
+++ problems/asteroids-collision.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/561/week-3-october-15th-october-21st/3502/
+
+// Non-Stack Approach
+// TC: O(n)
+// var asteroidCollision = function(asteroids) {
+//     for (let i = asteroids.length - 1; i >= 1;) {
+//         console.log(asteroids[i - 1], asteroids[i], i);
+//         if (asteroids[i] < 0 && asteroids[i - 1] > 0 && asteroids[i] < asteroids[i -1]) {
+//             (asteroids[i] * -1 > asteroids[i -1]) ? asteroids.splice(i - 1, 1) : ((asteroids[i] * -1 === asteroids[i -1]) ? asteroids.splice(i - 1, 2) : asteroids.splice(i, 1));
+//         } else {
+//             i--;
+//         }
+//     }
+
+//     return asteroids;
+// };
+
+// Stack Approach
+var asteroidCollision = function(asteroids) {
+
+}
+
+console.log(asteroidCollision([-2,-1,1,2]));    // [-2,-1,1,2]
+console.log(asteroidCollision([5, 10, -5]));    // [5, 10]
+// console.log(asteroidCollision([10,2,-5]));  // [10]
+// console.log(asteroidCollision([-2,2,-1,-2]));  // [-2]
---
+++ problems/excel-column-title.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/excel-sheet-column-title/
+
+var convertToTitle = function(n) {
+    
+};
+
+console.log(convertToTitle(27));    // AA
+console.log(convertToTitle(26));    // Z
+console.log(convertToTitle(705));   // AAC
