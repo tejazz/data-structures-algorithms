4c2a171c2e70b9b6f9149a4842ee396d37a90d60
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2020-10-24 01:14:04 +0700

    duplicates question

---
+++ problems/contains-duplicate-iii.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/contains-duplicate-iii/
+
+var containsNearbyAlmostDuplicate = function(nums, k, t) {
+    
+};
+
+console.log(containsNearbyAlmostDuplicate([1,2,3,1], 3, 0));    // true
+console.log(containsNearbyAlmostDuplicate([1,0,1,1], 1, 2));    // true
+console.log(containsNearbyAlmostDuplicate([1,5,9,1,5,9], 2, 3));    // false
