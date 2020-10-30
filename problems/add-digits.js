4410de6552a70f82044c0f465ea209bb669f1496
tejaz <Tarique.Ejaz@agoda.com>
2020-10-30 15:03:01 +0700

    add digits

--- problems/add-digits.js
+++ problems/add-digits.js
@@ -1,8 +1,23 @@
 // https://leetcode.com/problems/add-digits/
 
-var addDigits = function(num) {
+var addDigits = function (num) {
+    while (num > 9) {
+        num = getNumberSum(num);
+    }
 
+    return num;
 };
+
+function getNumberSum(num) {
+    let sum = 0;
+
+    while (num > 0) {
+        sum += Math.floor(num % 10);
+        num = Math.floor(num / 10);
+    }
+
+    return sum;
+}
 
 console.log(addDigits(38)); // 2
 console.log(addDigits(42)); // 6
