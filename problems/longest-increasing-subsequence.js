afed81cab692da9dc4956ac8ef5c0fda258c110b
tejaz <Tarique.Ejaz@agoda.com>
2020-10-30 18:15:32 +0700

    added daily question

---
+++ problems/longest-increasing-subsequence.js
@@ -0,0 +1,7 @@
+// https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/563/week-5-october-29th-october-31st/3513/
+
+var findNumberOfLIS = function (nums) {
+};
+
+console.log(findNumberOfLIS([1, 3, 5, 4, 7])); // 2
+// console.log(findNumberOfLIS([2, 2, 2, 2, 2])); // 5
