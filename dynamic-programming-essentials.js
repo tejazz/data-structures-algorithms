ce5a569c03004eeccdf60e37861c4b0edef3efe3
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2021-05-20 10:49:20 +0700

    feat: add cansum memoization technique

--- dynamic-programming-essentials.js
+++ dynamic-programming-essentials.js
@@ -61,4 +61,72 @@
 
 // console.log(gridTraveler(18, 18));
 
+// -------------------------
+
+/*
+    Memoization Recipe
+
+    1. Make it work
+    - Visualize the problems as a tree
+    - Implement tree using recursion
+    - Test it
+
+    2. Make it efficient
+    - Add memo object
+    - Add base case to return memo values
+    - Store return values into memo
+*/
+
+// -------------------------
+
+// canSum => Memoization
+
+// Write a function canSum(targetSum, numbers) that takes in a targetSum and an array of values
+// It should return a boolean if it possible or not to generate the targetSum using numbers in array
+
+// General
+// TC: O(n^m), SC: O(m)
+
+// const canSum = (targetSum, numbers) => {
+//     if (targetSum === 0) return true;
+//     if (targetSum < 0) return false;
+
+//     for (let i = 0; i < numbers.length; i++) {
+//         let remainder = targetSum - numbers[i];
+
+//         if (canSum(remainder, numbers)) return true;
+//     }
+
+//     return false;
+// };
+
+// Memoized
+// TC: O(m*n), SC: O(m)
+
+// const canSum = (targetSum, numbers, memo = {}) => {
+//     if (targetSum in memo) return memo[targetSum];
+//     if (targetSum === 0) return true;
+//     if (targetSum < 0) return false;
+
+//     for (let i = 0; i < numbers.length; i++) {
+//         let remainder = targetSum - numbers[i];
+//         memo[targetSum] = canSum(remainder, numbers, memo);
+
+//         if (memo[targetSum]) {
+//             return true; 
+//         }
+//     }
+
+//     memo[targetSum] = false;
+//     return false;
+// };
+
+// console.log(canSum(7, [5, 3, 4, 7]));  // true
+// console.log(canSum(7, [2, 4]));  // false
+// console.log(canSum(300, [7, 14]));  // false
+
+// -------------------------
+
+// howSum - Memoization
+
 
