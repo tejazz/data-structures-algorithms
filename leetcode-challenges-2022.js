42c3c6c8a9cfda9540ab6cc4cea6ceefa2252359
Tarique <tariqueejaz.93102@gmail.com>
2022-01-02 14:57:46 +0100

    chore: add new problems

---
+++ leetcode-challenges-2022.js
@@ -0,0 +1,10 @@
+/*
+
+Leetcode Challenges 2022
+
+January 
+
+1. Burst Balloons (https://leetcode.com/problems/burst-balloons/) - (Revisit)
+2. Pair of Songs With Total Durations Divisible by 60 (https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/) - (Revisit)
+
+*/
